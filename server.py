52ff3b633cc028b4264418e1c071cbaf541aa96d
Your Name <you@example.com>
2020-01-22 12:58:36 -0800

    Flask intro up till further study

--- server.py
+++ server.py
@@ -18,7 +18,15 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+      <body>
+        Hi! This is blah the home page.
+        Go to the hello page: <a href="/hello">My hello page!</a>
+      </body>
+    </html>
+    """
 
 
 @app.route("/hello")
@@ -35,7 +43,22 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
+          <input type="submit" value="Submit"><br>
+          Choose from a compliment:
+          <select name='compliment'>
+            <option value='awesome'>awesome</option>
+            <option value='brilliant'>brilliant</option>
+            <option value='lovely'>lovely</option>
+          </select>
+        </form>
+        <form action="/diss">
+        <input type="submit" value="Submit">
+        Select a diss:
+          <select name="insult">
+             <option value='dumb'>dumb</option>
+            <option value='mean'>mean</option>
+            <option value='chicken'>chicken</option>
+          </select>
         </form>
       </body>
     </html>
@@ -48,10 +71,9 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
-    y = x
-
+    
     return """
     <!doctype html>
     <html>
@@ -65,7 +87,24 @@ def greet_person():
     """.format(player, compliment)
 
 
+@app.route("/diss")
+def insults():
+  insult = request.args.get("insult")
+  
+  return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>An Insult</title>
+      </head>
+      <body>
+        Hi! I think you're {}!
+      </body>
+    </html>
+    """.format(insult)
+
+
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
